27f898b99ec26698309f687ba9e9e0f7839cadd8
orthagonal <orthagonal1@gmail.com>
2019-06-18 22:16:57 +0000

    ttl test

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const MemoryCache = require('@firstandthird/memory-cache');
 
 class FunctionCache {
-  constructor(allowStale) {
+  constructor(fastMode = false) {
     this.cache = new MemoryCache();
-    this.allowStale = allowStale;
+    this.fastMode = fastMode;
   }
 
   async memo(key, fn, ttl, forceUpdate) {
     const value = this.cache.getCacheObject(key);
     if (!forceUpdate && value) {
-      // in allowStale mode we let it refresh
-      // in the background while the current value is returned:
-      if (this.allowStale) {
-        new Promise(async resolve => {
-          const result = await fn();
-          this.cache.set(key, result, ttl);
-        });
+      // in fastMode we refresh expired values in the
+      // background while the current value is returned:
+      if ((value.expires > 0 && value.expires < new Date().getTime())) {
+        if (this.fastMode) {
+          new Promise(async resolve => {
+            const result = await fn();
+            this.cache.set(key, result, ttl);
+          });
+          return value.value;
+        }
+      } else {
+        return value.value;
       }
-      return value.value;
     }
     const result = await fn();
     this.cache.set(key, result, ttl);
