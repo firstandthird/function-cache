92971a9345242741337764985c87482f9105dafb
orthagonal <orthagonal1@gmail.com>
2019-06-18 16:38:22 +0000

    fix comments

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const MemoryCache = require('@firstandthird/memory-cache');
 
-const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-
 class FunctionCache {
   constructor(allowStale) {
     this.cache = new MemoryCache();
     this.allowStale = allowStale;
   }
 
-  // returns an instance of a memoizer:
   async memo(key, fn, ttl, forceUpdate) {
     const value = this.cache.getCacheObject(key);
     if (!forceUpdate && value) {
       // in allowStale mode we let it refresh while the current value is returned:
       if (this.allowStale) {
-        // don't await this so it can resolve in the background
-        // while we move on and return the current value:
+        // resolve in the background while we move on and return the current value:
         new Promise(async resolve => {
           const result = await fn();
           this.cache.set(key, result, ttl);
